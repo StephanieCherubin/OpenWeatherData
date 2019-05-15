b174ffa134d028ddf8c027904ef62cd9f00cb520
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-05-14 23:29:30 -0700

    edit based on style guide

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,16 +1,48 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './Temperature.css';
 
-function Temperature(props) {
-    const { temp, temp_min, temp_max } = props.weatherData.main 
+function Temperature(weatherData) {
+  const { temp, tempMin, tempMax } = weatherData.main;
 
-    return (
-        <div>
-          <div>Temperature: { Math.ceil ( 9/5 * (temp - 273) + 32 ) } F / {Math.ceil (temp -273.15)} C</div>
-          <div>Temperature Lows:{ Math.ceil ( 9/5 * (temp_min - 273) + 32 ) } F / {Math.ceil (temp_min -273.15)} C </div>
-          <div>Temperature Highs: { Math.ceil ( 9/5 * (temp_max - 273) + 32 ) } F / {Math.ceil (temp_max -273.15)} C </div>
-        </div>
-      )
+  return (
+    <div>
+      <div>
+Temperature:
+        {' '}
+        { Math.ceil(9 / 5 * (temp - 273) + 32) }
+        {' '}
+F /
+        {' '}
+        {Math.ceil(temp - 273.15)}
+        {' '}
+C
+      </div>
+      <div>
+Temperature Lows:
+        { Math.ceil(9 / 5 * (tempMin - 273) + 32) }
+        {' '}
+F /
+        {' '}
+        {Math.ceil(tempMin - 273.15)}
+        {' '}
+C
+        {' '}
+      </div>
+      <div>
+Temperature Highs:
+        {' '}
+        { Math.ceil(9 / 5 * (tempMax - 273) + 32) }
+        {' '}
+F /
+        {' '}
+        {Math.ceil(tempMax - 273.15)}
+        {' '}
+C
+        {' '}
+      </div>
+    </div>
+  );
 }
 
-export default Temperature
+export default Temperature;
